dd79d947d65a65ba2130c98b643bdb8677a02f76
lynn-richards <62219079+lynn-richards@users.noreply.github.com>
2020-03-30 12:21:56 -0300

    Update Script_Clase03.R

--- Class_03/Script_Clase03.R
+++ Class_03/Script_Clase03.R
@@ -13,7 +13,7 @@ library(readxl)
 
 casos<-data.table(read_excel("Class_02/2020-03-17-Casos-confirmados.xlsx",na = "—",trim_ws = TRUE,col_names = TRUE),stringsAsFactors = FALSE)
 
-casos<-casos[Región=="Metropolitana",]
+casos<-casos[Región=="Metropolitana",] #
 
 saveRDS
 
