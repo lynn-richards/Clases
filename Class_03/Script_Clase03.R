220be39c8941c2bcc529641b1e00c87d4dd3abfc
lynn-richards <62219079+lynn-richards@users.noreply.github.com>
2020-04-02 09:54:04 -0300

    Arreglos script

--- Class_03/Script_Clase03.R
+++ Class_03/Script_Clase03.R
@@ -10,15 +10,17 @@
 # Reading an exporting data
 
 library(readxl)
-
+library(data.table)
 casos<-data.table(read_excel("Class_02/2020-03-17-Casos-confirmados.xlsx",na = "—",trim_ws = TRUE,col_names = TRUE),stringsAsFactors = FALSE)
 
 casos<-casos[Región=="Metropolitana",] #
 
-saveRDS
+saveRDS(casos,"Class_03/CasosCovid_RM.rds")
 
 write.csv(casos,file = 'Class_03/CasosCovid_RM.csv',fileEncoding = 'UTF-8')
 
+#install.packages("writexl")
+library(writexl)
 writexl::write_xlsx
 
 library(foreign)
@@ -31,14 +33,16 @@ casosRM[,table(Sexo)]
 casosRM[Sexo=="Fememino",Sexo:="Femenino"]
 
 
-# Creating (factor) variables
+# Creating (factor) variables, variables que son str pero tienen leyendas
+
 
 class(casosRM$Sexo)
-
+casosRM[,Sexo:=factor(Sexo)]
 casosRM[,Sexo:=factor(Sexo,nmax = 2)]
 
 head(casosRM$Sexo)
 head(as.numeric(casosRM$Sexo))
+levels(casosRM$Sexo)
 
 table(casosRM$Sexo)
 casosRM[,.N,by=.(Sexo)]
@@ -46,31 +50,37 @@ casosRM[,.N,by=.(Sexo,`Centro de salud`)]
 
 #Collapsing by Centro de Salud 
 
-casosRM[,sum(`Casos confirmados`,na.rm = T),by=.(`Centro de salud`)][,V1/sum(V1)]
+casosRM[,sum(`Casos confirmados`,na.rm = T),by=.(`Centro de salud`)][,V1/sum(V1)] #me entrega los porcentajes de los casos confirmados por clinica en la region metropolitana.
+obj1<-casosRM[,mean(`Casos confirmados`,na.rm = T),by=.(`Centro de salud`)]
+obj1[,V1/sum(V1,na.rm = T)]
 
 # collapsing by average age
+casosRM[`Casos confirmados`]
 
 A<-casosRM[,.(AvAge=mean(Edad,na.rm = T)),by=.(`Centro de salud`)]
 
-B<-casosRM[,.(Total_centro=sum(`Casos confirmados`,na.rm = T)),by=.(`Centro de salud`)]
+B<-casosRM[,.(Total_centro=.N),by=.(`Centro de salud`)]
 
-C<-casosRM[Sexo=="Femenino",.(Total_Centro_Mujeres=sum(`Casos confirmados`,na.rm = T)),by=.(`Centro de salud`)]
+C<-casosRM[Sexo=="Femenino",.(Total_Centro_Mujeres=.N),by=.(`Centro de salud`)]
 
-D<-casosRM[Sexo=="Masculino",.(Total_Centro_Hombres=sum(`Casos confirmados`,na.rm = T)),by=.(`Centro de salud`)]
+D<-casosRM[Sexo=="Masculino",.(Total_Centro_Hombres=.N),by=.(`Centro de salud`)]
+dim(A)
 
 
 #merging data sets
-
-AB<-merge(A,B,by = "Centro de salud",all = T,sort = F)
+AB_fake<-cbind(A,B$Total_centro)
+AB<-merge(A,B,by ="Centro de salud",all = T,sort = F)
 ABC<-merge(AB,C,by = "Centro de salud",all = T,sort = F)
 ABCD<-merge(ABC,D,by = "Centro de salud",all = T,sort = F)
-
+ABCD[is.na(porc_mujeres),porc_mujeres:=0]
 ABCD[,porc_mujeres:=Total_Centro_Mujeres/Total_centro]
 
 # reshaping
 
 E<-casosRM[,.(AvAge=mean(Edad,na.rm = T),`Casos confirmados`=sum(`Casos confirmados`,na.rm = T)),by=.(`Centro de salud`,Sexo)]
-G<-reshape(E,direction = 'wide',timevar = 'Sexo',v.names = c('AvAge','Casos confirmados'),idvar = 'Centro de salud')
+E[`Centro de salud`== "Clinica Alemana", `Centro de salud`:="Clínica Alemana"]
+G<-reshape(E,direction = 'wide',timevar = 'Sexo',v.names = c('AvAge','Casos confirmados'),idvar = 'Centro de salud') #redistribuye el resultado, wide direccion hacia el lado, , average age quiero que ea ancha y los casos confirmados 
+
 
 #---- Part 2: Visualization  -------------------
 
@@ -80,10 +90,12 @@ plot(G$`Casos confirmados.Femenino`,G$`Casos confirmados.Masculino`)
 text(x =G$`Casos confirmados.Femenino`,y=G$`Casos confirmados.Masculino`, G$`Centro de salud`,cex=0.5)
 
 #ggplot2
+library(ggplot2)
 p1<-ggplot(G,aes(x=`Casos confirmados.Femenino`,y=`Casos confirmados.Masculino`))+geom_point(aes(size=AvAge.Femenino,colour=AvAge.Masculino))+geom_text(aes(label=`Centro de salud`),size=2,check_overlap = T)
 p1
 
 #plotly
+#install.packages("plotly")
 library(plotly)
 ggplotly(p1)
 
@@ -94,10 +106,12 @@ ggplotly(p1)
 
 
 #---- Part 3: Intro to Mapping  -------------------
-#install.packages("chilemapas")
+install.packages("chilemapas")
 library(chilemapas)
 library(data.table)
 library(ggplot2)
+
+View(mapa_zonas)
 
 zonas_censo<-data.table(censo_2017_zonas,stringsAsFactors = F)
 
@@ -114,7 +128,7 @@ zonas_valparaiso<-merge(zonas_valparaiso,poblacion_adulto_mayor_zonas,by="geocod
 
 #plotting
 library(RColorBrewer)
-paleta <- rev(brewer.pal(n = 9,name = "Reds"))
+paleta <- rev(brewer.pal(n = 9,name = "PuRd"))
 
 
 ggplot(zonas_valparaiso) + 
@@ -138,3 +152,4 @@ ggplot(zonas_valparaiso2) +
 hist(zonas_valparaiso$AdultosMayores,main = "Histograma Adultos Mayores Viña-Valpo")
 
 hist(zonas_valparaiso2$AdultosMayores,main = "Histograma Adultos Mayores Viña-Valpo")
+
